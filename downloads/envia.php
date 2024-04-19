53cace87e981f3fd9784828f551d88993066e989
Otavio Cezar Fernandes Sarcinelli <143614819+otaviocezar095@users.noreply.github.com>
2024-04-19 17:57:16 -0300

    Add files via upload

---
+++ downloads/envia.php
@@ -0,0 +1,19 @@
+<?php
+    $nome = addslashes($_POST ['nome']);
+    $email = addslashes($_POST['e-mail']);
+    $celular = addslashes($_POST['celular']);
+
+    $para = "otaviocezar095@gmail.com";
+    $assunto = "Duvidas";
+
+    $corpo = "Nome: ".$nome."\n"."Email: ".$email."\n"."celular: ".$telefone;
+
+    $cabeca = "From otaviocezar095@gmail.com"."\n"."reply-to: ".$email."\n"."x=mailer:PHP/".phpversion();
+    
+    if(mail($para,$assunto,$corpo,$cabeca)){
+        echo("E-mail enviado com sucesso!");
+    }else{
+        echo("Houve um erro ao enviar o e-mail!");
+    }
+?>
+
